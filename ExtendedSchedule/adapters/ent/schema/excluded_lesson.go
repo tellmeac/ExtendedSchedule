6636f20b485f136edb939c0f2b62756d42e0b447
Alexander Lipatov <tellmeac@gmail.com>
2022-04-27 09:23:22 +0700

    feat: user config repository setup

--- ExtendedSchedule/adapters/ent/schema/excluded_lesson.go
+++ ExtendedSchedule/adapters/ent/schema/excluded_lesson.go
@@ -3,7 +3,9 @@ package schema
 import (
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
 	"github.com/google/uuid"
+	"tellmeac/extended-schedule/domain/entity"
 )
 
 // ExcludedLesson holds the schema definition for the ExcludedLesson entity.
@@ -16,7 +18,16 @@ func (ExcludedLesson) Fields() []ent.Field {
 	return []ent.Field{
 		field.UUID("id", uuid.UUID{}).Default(uuid.New),
 		field.UUID("UserID", uuid.UUID{}),
-		field.String("LessonRef"),
+		field.String("LessonID"),
+		field.Int("Position"),
+		field.Int("Weekday"),
+		field.JSON("Teacher", &entity.TeacherInfo{}),
+	}
+}
+
+func (ExcludedLesson) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("UserID"),
 	}
 }
 
