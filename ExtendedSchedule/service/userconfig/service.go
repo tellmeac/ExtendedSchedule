aa6c68981d49d828b6a1f32a404bb453d3578037
Alexander Lipatov <tellmeac@gmail.com>
2022-04-27 20:49:19 +0700

    feat: update repository method

--- ExtendedSchedule/service/userconfig/service.go
+++ ExtendedSchedule/service/userconfig/service.go
@@ -2,15 +2,11 @@ package userconfig
 
 import (
 	"context"
-	"tellmeac/extended-schedule/domain/aggregates"
-	"tellmeac/extended-schedule/domain/entity"
-
 	"github.com/google/uuid"
+	"tellmeac/extended-schedule/domain/aggregates"
 )
 
 type IService interface {
 	GetUserConfig(ctx context.Context, userID uuid.UUID) (aggregates.UserConfig, error)
-	UpdateJoinedGroups(ctx context.Context, userID uuid.UUID, desired entity.JoinedGroups) error
-	UpdateIgnoredLessons(ctx context.Context, userID uuid.UUID, desired []entity.ExcludedLesson) error
-	UpdateExtendedLessons(ctx context.Context, userID uuid.UUID, desired []entity.ExtendedLesson) error
+	UpdateUserConfig(ctx context.Context, userID uuid.UUID, desired aggregates.UserConfig) error
 }
