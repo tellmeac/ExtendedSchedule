72ac24efe2aedba4bdeb0794c8a87d378271c836
Alexander Lipatov <tellmeac@gmail.com>
2022-04-30 20:52:01 +0700

    fix: unused styles removed

--- web/src/SchedulePage/SchedulePage.tsx
+++ web/src/SchedulePage/SchedulePage.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import {WeekSchedule} from "./WeekSchedule";
 import {ScheduleController} from "./ScheduleController/ScheduleController";
 import {Container} from "react-bootstrap";
+import "./SchedulePage.css"
 
 export function SchedulePage() {
-    return <Container>
+    return <Container className={"schedule-page"}>
         <ScheduleController/>
         <WeekSchedule/>
     </Container>
