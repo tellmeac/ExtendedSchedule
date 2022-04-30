16c494808b8dd92b30582fd113ce3e3c111c1d93
Alexander Lipatov <tellmeac@gmail.com>
2022-04-30 14:05:09 +0700

    fix: optimize imports

--- web/src/Shared/Store/Root.ts
+++ web/src/Shared/Store/Root.ts
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
 import {scheduleSlice} from "./ScheduleSlice";
 import {uiSlice} from "./UISlice";
 
