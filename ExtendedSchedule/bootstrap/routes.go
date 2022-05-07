045f93c1f165c24b391a2d2d5218cf2006488f04
Alexander Lipatov <tellmeac@gmail.com>
2022-05-08 01:10:57 +0700

    feat: google token id validation provided

--- ExtendedSchedule/bootstrap/routes.go
+++ ExtendedSchedule/bootstrap/routes.go
@@ -4,13 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 	"tellmeac/extended-schedule/service/schedule"
 	"tellmeac/extended-schedule/service/userconfig"
+	"tellmeac/extended-schedule/utils/middleware"
 	"tellmeac/extended-schedule/utils/routes/health"
 )
 
 // bindRoutes binds api endpoints logically.
-func bindRoutes(s *gin.Engine, schedule *schedule.Endpoints, configs *userconfig.Endpoints) {
-	api := s.Group("api/")
-	health.Endpoints.Bind(api)
+func bindRoutes(engine *gin.Engine, schedule *schedule.Endpoints, configs *userconfig.Endpoints) {
+	health.Endpoints.Bind(engine)
+
+	api := engine.Group("api/")
+	api.Use(middleware.GoogleOAuth2())
 	schedule.Bind(api)
 	configs.Bind(api)
 }
