ec2a304c990593593ee4cd06c9299de8eb4dae26
Alexander Lipatov <tellmeac@gmail.com>
2022-05-07 12:29:45 +0700

    feat: auth setup (not works)

--- web/src/Shared/Api/Schedule.ts
+++ web/src/Shared/Api/Schedule.ts
@@ -3,13 +3,16 @@ import {format} from "date-fns";
 
 const ScheduleAPIBaseUrl = process.env.API_BASE_URL || "http://localhost:8080/api"
 
-export async function getUserSchedule(id_token: string, startTime: number, endTime: number): Promise<ScheduleDay[]> {
+export async function getUserSchedule(tokenId: string, startTime: number, endTime: number): Promise<ScheduleDay[]> {
     const start = format(new Date(startTime), "u-MM-dd")
     const end = format(new Date(endTime), "u-MM-dd")
 
     const url = `${ScheduleAPIBaseUrl}/schedule/groups/3c9f5a5d-ffca-11eb-8169-005056bc249c?start=${start}&end=${end}`
     const response = await fetch(url, {
         method: 'GET',
+        headers: {
+            "Authorization": `Bearer ${tokenId}`,
+        },
     });
 
     return (await response.json()) as ScheduleDay[]
