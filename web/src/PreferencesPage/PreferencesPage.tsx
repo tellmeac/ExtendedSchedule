91010a74974919b1ab912df50662397ba5879241
Alexander Lipatov <tellmeac@gmail.com>
2022-05-10 08:50:02 +0700

    fix: pref page setup

--- web/src/PreferencesPage/PreferencesPage.tsx
+++ web/src/PreferencesPage/PreferencesPage.tsx
@@ -1,8 +1,27 @@
-import {Container} from "react-bootstrap";
-import React from "react";
+import {Button, Container, Form} from "react-bootstrap";
+import React, {useState} from "react";
 
 export function PreferencesPage() {
+    const options = ["hello", "world"]
+
     return <Container>
-        Параметры расписания (TODO)
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Список групп</Form.Label>
+                <Form.Control as="select" multiple>
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary">
+                Submit
+            </Button>
+        </Form>
     </Container>
 }
