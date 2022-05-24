b90f28b8ea41fa04fc239699ad88a967b1a519a2
Alexander Lipatov <tellmeac@gmail.com>
2022-05-24 20:22:06 +0700

    feat: bad solution to refactor

--- web/src/PreferencesPage/PreferencesPage.tsx
+++ web/src/PreferencesPage/PreferencesPage.tsx
@@ -1,52 +1,79 @@
 import {Button, Container, Form, ListGroup} from "react-bootstrap";
-import React, {useState} from "react";
+import React, {ChangeEvent, useEffect, useMemo, useState} from "react";
 import {FacultyInfo, GroupInfo} from "../Shared/Models";
+import {useAppSelector} from "../Shared/Hooks";
+import {selectUserData} from "../Shared/Store";
+import {getAllFaculties, getFacultyGroups} from "./Api";
 
 
 export function PreferencesPage() {
-    const mockFaculties: FacultyInfo[] = [
-        {
-            id: "1",
-            name: "Прикладная математика и компьютерные науки"
-        },
-        {
-            id: "2",
-            name: "Прикладная математика и компьютерные науки"
-        }
-    ]
+    const user = useAppSelector(selectUserData)
+    const [userGroups, setUserGroups] = useState<GroupInfo[]>([])
 
-    const mockGroups = [
-        {
-            "id": "931901",
-            "name": "11"
-        },
-        {
-            "id": "931902",
-            "name": "12"
-        },
-        {
-            "id": "931903",
-            "name": "13"
-        }
-    ]
-
-    const mockInitialGroups = [
-        {
-            "id": "931901",
-            "name": "11"
-        },
-        {
-            "id": "777",
-            "name": "14"
-        },
-        {
-            "id": "666",
-            "name": "15"
-        }
-    ]
-
-    const [userGroups, setUserGroups] = useState<GroupInfo[]>(mockInitialGroups)
+    // for new group selection
+    const [faculties, setFaculties] = useState<FacultyInfo[]>([])
+    const [facultyGroups, setFacultyGroups] = useState<GroupInfo[]>([])
+    const [selectedFaculty, setSelectedFaculty] = useState<FacultyInfo | undefined>(undefined)
     const [selectedGroup, setSelectedGroup] = useState<GroupInfo | undefined>(undefined)
+
+    const [addGroupActionAllowed, setAddGroupActionAllowed] = useState<boolean>(false)
+
+    useEffect(()=>{
+        // TODO:
+        // get user's joined groups
+
+        setUserGroups([])
+    }, [user])
+
+    useEffect(()=>{
+        if (user === undefined) {
+            return
+        }
+
+        getAllFaculties(user?.tokenId || "").then(r => {
+            setFaculties(r)
+            setSelectedFaculty(r[0])
+        }).catch(err => {
+            console.error(err)
+        })
+    }, [])
+
+    useEffect(()=>{
+        if (user === undefined) {
+            return
+        }
+
+        if (selectedFaculty === undefined) {
+            return
+        }
+
+        getFacultyGroups(user?.tokenId || "", selectedFaculty.id || "").then(r => {
+            setFacultyGroups(r)
+        }).catch(err => {
+            console.error(err)
+        })
+    }, [user, selectedFaculty])
+
+    const selectFaculty = (event: ChangeEvent<HTMLSelectElement>) => {
+        setSelectedFaculty(parseFaculty(event.target.value))
+    }
+
+    const selectGroup = (event: ChangeEvent<HTMLSelectElement>) => {
+        setSelectedGroup(parseGroup(event.target.value))
+        setAddGroupActionAllowed(!isAddGroupActionDisabled())
+    }
+
+    const addGroup = () => {
+        if (!selectedGroup) {
+            return
+        }
+        setUserGroups([...userGroups, selectedGroup])
+        setAddGroupActionAllowed(false)
+    }
+
+    const isAddGroupActionDisabled = () => {
+        return selectedGroup === undefined || userGroups.find((g)=>{return g === selectedGroup}) !== undefined
+    }
 
     return <Container>
         <Form>
@@ -54,27 +81,27 @@ export function PreferencesPage() {
                 <Form.Label className="mb-3">Добавить группу в список</Form.Label>
                 <Form.Group className="mb-3">
                     <Form.Label>Факультет</Form.Label>
-                    <Form.Select aria-label=''>
+                    <Form.Select aria-label='факультеты' onChange={selectFaculty}>
                         {
-                            mockFaculties.map((faculty)=>{
-                                return <option id={faculty.value} value={faculty.value}>
-                                    {faculty.labelKey}
+                            faculties.map((faculty)=>{
+                                return <option key={faculty.name} value={faculty.id + "__" + faculty.name}>
+                                    {faculty.name}
                                 </option>
                             })
                         }
                     </Form.Select>
                     <Form.Label>Группа</Form.Label>
-                    <Form.Select aria-label='' onSelect={selectGroup}>
+                    <Form.Select aria-label='группы' onChange={selectGroup}>
                         {
-                            mockGroups.map((g)=>{
-                                return <option id={g.value} value={g.value}>
-                                    {g.labelKey}
+                            facultyGroups.map((g)=>{
+                                return <option key={g.name} value={g.id + "__" + g.name}>
+                                    {g.name}
                                 </option>
                             })
                         }
                     </Form.Select>
                 </Form.Group>
-                <Button variant="outline-primary" onClick={addGroup} disabled={selectedGroup === undefined}>
+                <Button variant="outline-primary" onClick={addGroup} disabled={addGroupActionAllowed}>
                     Добавить
                 </Button>
             </Form.Group>
@@ -82,9 +109,9 @@ export function PreferencesPage() {
                 <Form.Label>Список выбранных групп:</Form.Label>
                 <ListGroup>
                     {
-                        userGroups.map((g)=>{
-                            return <ListGroup.Item id={g.id}>
-                                {g.name}
+                        userGroups.map((g, ind)=>{
+                            return <ListGroup.Item key={g.name}>
+                                {ind+1}. {g.name}
                             </ListGroup.Item>
                         })
                     }
@@ -95,4 +122,20 @@ export function PreferencesPage() {
             </Button>
         </Form>
     </Container>
+}
+
+function parseFaculty(value: string): FacultyInfo {
+    const [id, name] = value.split("__", 2)
+    return {
+        id: id,
+        name: name
+    }
+}
+
+function parseGroup(value: string): GroupInfo {
+    const [id, name] = value.split("__", 2)
+    return {
+        id: id,
+        name: name
+    }
 }
