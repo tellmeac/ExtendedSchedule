28a8b8bc199820e4a90c3e712dc06e916f0bc991
Lipatov Alexander <tellmeac@gmail.com>
2022-06-01 19:21:02 +0700

    fix: dead code removed

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {store} from "./Shared/Store";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
