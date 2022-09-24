ede9d478404c4f7644883a5695aa02208984b0eb
Alexander <tellmeac@gmail.com>
2022-09-24 12:31:03 +0700

    refactor: models for userconfig api

--- schedule/common/userconfig/generate.go
+++
@@ -1,7 +0,0 @@
-package userconfig
-
-import (
-	_ "github.com/deepmap/oapi-codegen/pkg/codegen"
-)
-
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=config.yaml ../../../api/UserConfig.yaml
